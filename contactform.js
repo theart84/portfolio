190442e7754f8c63c0d99f2e662dfc0f8446be39
theart84 <64737975+theart84@users.noreply.github.com>
2020-06-28 02:12:29 +0500

    Add files via upload

---
+++ contactform.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function($) {
+
+$(".form").submit(function() {
+var str = $(this).serialize();
+
+$.ajax({
+type: "POST",
+url: "contact.php",
+data: str,
+success: function(msg) {
+if(msg == 'OK') {
+result = '<p>Thank You!</p>';
+$(".fields").hide();
+} else {
+result = msg;
+}
+$('.note').html(result);
+}
+});
+return false;
+});
+});
